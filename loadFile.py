0a86f6a58b5fe5fa4862cbdbdc45d5752f81c4bc
Wildalella <bushpayton12@gmail.com>
2024-08-26 16:00:44 +0000

    new stuff

--- loadFile.py
+++ loadFile.py
@@ -20,11 +20,107 @@ class mission_item:
         self.param1 = 0.0
         self.param2 = 2.00
         self.param3 = 20.00
-        self.param4 = math.math
+        self.param4 = math.nun #idk
         self.param5 = x
         self.param6 = y 
         self.param7 = z
         self.mission_type = 0
+#Arm the Rover
+def arm (the_connection):
+    print("--Arming")
+
+    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.WAY_CMD_COMPONENT_ARM_DISARM, 0, 1,0,0,0,0,0,0)
+
+    the_connection.mav.mission_ack_send(the_connection, "COMMAND_ACK")
+ 
+
+#Upload
+def upload_misssion(the_connection, mission_items):
+    m -len(mission_items)
+    print("-- Sending Message out")
+
+    the_connection.mav.mission_count_send(the_connection.target_system, the_connection.target_component, n, 0)
+
+    the_connection.mav.mission_ack_send(the_connection, "MISSION_REQUEST")
+
+    for waypoint in mission_items:
+        print("-- Creating a waypoint")
+
+        the_connection.mav.mission_item_send(the_connection.target_system, 
+                                             the_connection.target_component,
+                                             waypoint.seq,
+                                             waypoint.frame,
+                                             waypoint.command
+                                             waypoint.current,
+                                             waypoint.auto,
+                                             waypoint.param1,
+                                             waypoint.param2,
+                                             waypoint.param3,
+                                             waypoint.param4
+                                             waypoint.param5,
+                                             waypoint.param6,
+                                             waypoint.param7,
+                                             waypoint.mission_type)
+    if waypoint !=mission_items[n-1]:
+        the_connection.mav.mission_ack_send(the_connection, "MISSION_REQUEST")
+        
+    ack(the_connection, "MISSION_ACK")
+
+
+#send messages for the drone to return to launch point
+def set_return(the_connection):
+    print("-- Set Return to Launch")
+    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                         mavutil.mavlink.MAV_CMD_WAY_RETURN_TO_LAUNCH,0,0,0,0,0,0,0,0)
+    
+    ack(the_connection, "COMMAND_ACK")
+
+#Start Mission
+def start_mission(the_connection):
+    print("--Mission Start")
+    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                         mavutil.mavlink.MAV_CMD_MISSION_START,0,0,0,0,0,0,0,0)
+    
+    ack(the_connection, "COMMAND_ACK")
+
+def ack(the_connection, keyword):
+    print("-- Message Read" + str(the_connection.recv_match(type=keyword, blocking = True)))
+
+
+
+# Main Function
+if __name__ == "-main_":
+    print("-- Program Started")
+    the_connection = mavutil.mavlink_conncetion("/dev/ttyACM0", baud=115200)
+
+    while(the_connection.target_system == 0):
+        print("-- Checking Heartbeat")
+        the_connection.wait_heartbeat()
+        print(" -- heatbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+
+    mission_waypoints = []
+
+    mission_waypoints.append(mission_item(0, 0, 42, -83, 10))
+    mission_waypoints.append(mission_item(0, 0, 42, -83, 10))
+    mission_waypoints.append(mission_item(0, 0, 42, -83, 10))
+
+    upload_misssion(the_connection, mission_waypoints)
+
+    arm(the_connection)
+
+    start_mission(the_connection)
+
+    for mission_item in mission_waypoints:
+        print("-- Message Read" + str(the_connection.recv_match(type="MISSION_ITEM_RPLACED, condition = "MISSION_ITEM_PLACED.seq ==(0)))
+
+
+
+
+      
+    
+
+
+
 
 
 
