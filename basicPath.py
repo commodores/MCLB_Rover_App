fed9eaa314e1eb0cdbd71d9bf9518874b6c0ed8e
Wildalella <bushpayton12@gmail.com>
2024-08-27 17:34:23 +0000

    idk what im doing

--- basicPath.py
+++ basicPath.py
@@ -1,18 +1,12 @@
-# Import mavutil
+import sys
 from pymavlink import mavutil
-
-
 # Create the connection
 master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 # Wait a heartbeat before sending commands
 master.wait_heartbeat()
 
-
-master.mav.send(mavutil.mavlink.MAVLink_set_position_target_global_int_message(10, master.target_system,
-                        master.target_component, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, int(0b110111111000), int(-35.3629849 * 10 ** 7), int(149.1649185 * 10 ** 7), 10, 0, 0, 0, 0, 0, 0, 1.57, 0.5))
-
-
-while 1:
-    msg = master.recv_match(
-        type='LOCAL_POSITION_NED', blocking=True)
-    print(msg)
+while True:
+    msg = master.recv_match(type = 'HEARTBEAT', blocking = False)
+    if msg:
+        mode = mavutil.mode_string_v10(msg)
+        print(mode)                                                                                                                                                                                                                                                                                                                                                                            
--- loadFile.py
+++ loadFile.py
@@ -4,15 +4,15 @@ from pymavlink import mavutil
 
 
 # Create the connection
-master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+#master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 # Wait a heartbeat before sending commands
-master.wait_heartbeat()
+#master.wait_heartbeat()
 
 
 #Class for formatting the Mission Ite,
 class mission_item:
-    def _init_(self,  current, x,y,z):
-        self.seq = 1
+    def __init__(self, seq, current, x, y, z):
+        self.seq = seq
         self.frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
         self.command = mavutil.mavlink.MAV_CMD_NAV_WAYPOINT
         self.current = current
@@ -20,47 +20,37 @@ class mission_item:
         self.param1 = 0.0
         self.param2 = 2.00
         self.param3 = 20.00
-        self.param4 = math.nun #idk
+        self.param4 = math.nan #idk
         self.param5 = x
         self.param6 = y 
         self.param7 = z
         self.mission_type = 0
+
 #Arm the Rover
 def arm (the_connection):
     print("--Arming")
 
-    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.WAY_CMD_COMPONENT_ARM_DISARM, 0, 1,0,0,0,0,0,0)
+    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1,0,0,0,0,0,0)
 
-    the_connection.mav.mission_ack_send(the_connection, "COMMAND_ACK")
+    ack(the_connection, "COMMAND_ACK")
  
 
-#Upload
+#Upload Mission
 def upload_misssion(the_connection, mission_items):
-    m -len(mission_items)
+    n = len(mission_items)
     print("-- Sending Message out")
 
     the_connection.mav.mission_count_send(the_connection.target_system, the_connection.target_component, n, 0)
 
-    the_connection.mav.mission_ack_send(the_connection, "MISSION_REQUEST")
+    #ack(the_connection, "MISSION_REQUEST")
 
     for waypoint in mission_items:
         print("-- Creating a waypoint")
 
-        the_connection.mav.mission_item_send(the_connection.target_system, 
-                                             the_connection.target_component,
-                                             waypoint.seq,
-                                             waypoint.frame,
-                                             waypoint.command
-                                             waypoint.current,
-                                             waypoint.auto,
-                                             waypoint.param1,
-                                             waypoint.param2,
-                                             waypoint.param3,
-                                             waypoint.param4
-                                             waypoint.param5,
-                                             waypoint.param6,
-                                             waypoint.param7,
-                                             waypoint.mission_type)
+        the_connection.mav.mission_item_send(the_connection.target_system, the_connection.target_component, waypoint.seq, waypoint.frame, waypoint.command, waypoint.current, waypoint.auto, waypoint.param1, waypoint.param2, waypoint.param3, waypoint.param4, waypoint.param5,
+                                                waypoint.param6,
+                                                waypoint.param7,
+                                                waypoint.mission_type)
     if waypoint !=mission_items[n-1]:
         the_connection.mav.mission_ack_send(the_connection, "MISSION_REQUEST")
         
@@ -83,15 +73,16 @@ def start_mission(the_connection):
     
     ack(the_connection, "COMMAND_ACK")
 
+#acknowledge func
 def ack(the_connection, keyword):
-    print("-- Message Read" + str(the_connection.recv_match(type=keyword, blocking = True)))
+    print("-- Message Read " + str(the_connection.recv_match(type=keyword, blocking =True)))
 
 
 
 # Main Function
-if __name__ == "-main_":
+if __name__ == "__main__":
     print("-- Program Started")
-    the_connection = mavutil.mavlink_conncetion("/dev/ttyACM0", baud=115200)
+    the_connection = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 
     while(the_connection.target_system == 0):
         print("-- Checking Heartbeat")
@@ -100,9 +91,9 @@ if __name__ == "-main_":
 
     mission_waypoints = []
 
-    mission_waypoints.append(mission_item(0, 0, 42, -83, 10))
-    mission_waypoints.append(mission_item(0, 0, 42, -83, 10))
-    mission_waypoints.append(mission_item(0, 0, 42, -83, 10))
+    mission_waypoints.append(mission_item(0, 0, 31.5559748, -84.1694355, 0))
+    #mission_waypoints.append(mission_item(1, 0, 42, -83, 0))
+    #mission_waypoints.append(mission_item(2, 0, 42, -83, 0))
 
     upload_misssion(the_connection, mission_waypoints)
 
@@ -111,8 +102,9 @@ if __name__ == "-main_":
     start_mission(the_connection)
 
     for mission_item in mission_waypoints:
-        print("-- Message Read" + str(the_connection.recv_match(type="MISSION_ITEM_RPLACED, condition = "MISSION_ITEM_PLACED.seq ==(0)))
+        print("-- Message Read " + str(the_connection.recv_match(type="MISSION_ITEM_REACHED", condition= "MISSION_ITEM_REACHED.seq =={0}".format(mission_item.seq), blocking =True)))
 
+    set_return(the_connection)
 
 
 
